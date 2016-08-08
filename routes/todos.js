21451ab292bde9ff8de501d2b46584cee5fdc2cd
Sam <samgitrepos@gmail.com>
2016-08-08 07:21:26 +0100

    Just a test

--- routes/todos.js
+++ routes/todos.js
@@ -14,13 +14,29 @@ var todos =[
 
 router.get('/' , function (req, res) {
 
-    res.send('The TODO app');
+    res.render('todo', {title : 'TODO APP'});
 });
 
 
 router.get('/todo' , function (req, res) {
 
     res.json(todos);
+});
+
+router.get('/todo/:id', function (req, res) {
+    var matchTodo = parseInt(req.params.id, 10);
+    var matchItems;
+    todos.forEach(function (match) {
+        if(matchTodo === match.id){
+            matchItems = match;
+        }
+    });
+    if(matchItems){
+        res.json(matchItems);
+    }
+    else {
+        res.status(404).send();
+    }
 });
 
 module.exports = router;
