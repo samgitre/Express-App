7d6084dcaafc8cd8a89189c1d232188791f52dbb
Sam <samgitrepos@gmail.com>
2016-08-14 04:43:14 +0100

    postgre configuration

--- db.js
+++ db.js
@@ -2,11 +2,19 @@
  * Created by Samson on 8/13/2016.
  */
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === 'production'){
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect : 'postgres'
+    });
+}else{
+    sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect' : 'sqlite',
     'storage' : __dirname + '/data/todoData.sqlite'
 });
-
+}
 var db = {};
 db.todo = sequelize.import(__dirname + '/model/todo.js');
 db.sequelize = sequelize;
